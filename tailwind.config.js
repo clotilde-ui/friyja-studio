c6f9f65009839ecc3223073229abe21780fe1c01
clotilde-ui <clotilde@deux.io>
2025-11-22 12:26:09 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,19 @@ export default {
         sans: ['Inter', 'sans-serif'],
       },
       colors: {
+        // Redéfinition complète selon votre charte
+        white: '#FAF5ED', // Le "Nouveau Blanc" est Beige
+        black: '#232323', // Le "Nouveau Noir" est Anthracite
+        
         studio: {
-          bg: '#FAF5ED',      // FOND APP (Crème)
-          text: '#232323',    // TEXTE PRINCIPAL (Noir Freyja)
+          bg: '#FAF5ED',      // FOND APP
+          text: '#232323',    // TEXTE PAGE
           
-          card: '#232323',    // FOND CARTES (Noir Freyja)
-          cardText: '#FAF5ED',// TEXTE SUR CARTES (Crème)
+          card: '#232323',    // FOND CARTES
+          cardText: '#FAF5ED',// TEXTE CARTES
           
-          accent: '#24B745',  // VERT FREYJA
-          pink: '#FFBEFA',    // Rose
+          accent: '#24B745',  // VERT
+          accentHover: '#1f9e3b',
         }
       }
     },
